ee8b9a98f987f18a6f06ba9e0b921bf5c05ae56f
Mikheil Meskhi <mishameskhi1@gmail.com>
2022-08-01 21:30:23 +0400

    add game, timer, start btn

--- src/js/app.js
+++ src/js/app.js
@@ -14,8 +14,115 @@ for (let i = 0; i < table.length; i++) {
     div.style.left = j * 100 + "px";
     div.dataset.i = i;
     div.dataset.j = j;
-    document.getElementById("table").appendChild(div);
+    document.getElementById("table-a").appendChild(div);
   }
 }
 
 const btn = document.getElementById("btn");
+
+function start() {
+  document.getElementById("table-a").style.display = "none";
+  document.getElementById("table-b").style.display = "unset";
+}
+
+function stop() {
+  document.getElementById("table-a").style.display = "";
+  document.getElementById("table-b").style.display = "none";
+  document.getElementById("timer").style.display = "none";
+  document.getElementById("timer-stop").style.display = "revert";
+}
+
+btn.addEventListener("click", start);
+btn.addEventListener("click", timerStart);
+
+///////////
+
+let clickedCard = null;
+let preventClick = false;
+let combosFound = 0;
+
+const colors = [
+  "pink",
+  "yellow",
+  "red",
+  "blue",
+  "teal",
+  "orange",
+  "green",
+  "lightblue",
+  "brown",
+  "violet",
+  "purple",
+  "gold",
+];
+
+const cards = [...document.querySelectorAll(".card")];
+for (let color of colors) {
+  const cardAIndex = parseInt(Math.random() * cards.length);
+  const cardA = cards[cardAIndex];
+  cards.splice(cardAIndex, 1);
+  cardA.className += ` ${color}`;
+  cardA.setAttribute("data-color", color);
+
+  const cardBIndex = parseInt(Math.random() * cards.length);
+  const cardB = cards[cardBIndex];
+  cards.splice(cardBIndex, 1);
+  cardB.className += ` ${color}`;
+  cardB.setAttribute("data-color", color);
+}
+
+function onCardClicked(e) {
+  const target = e.currentTarget;
+
+  if (
+    preventClick ||
+    target === clickedCard ||
+    target.className.includes("done")
+  ) {
+    return;
+  }
+
+  target.className = target.className.replace("color-hidden", "").trim();
+  target.className += " done";
+
+  if (!clickedCard) {
+    clickedCard = target;
+  } else if (clickedCard) {
+    if (
+      clickedCard.getAttribute("data-color") !==
+      target.getAttribute("data-color")
+    ) {
+      preventClick = true;
+      setTimeout(() => {
+        clickedCard.className =
+          clickedCard.className.replace("done", "").trim() + " color-hidden";
+        target.className =
+          target.className.replace("done", "").trim() + " color-hidden";
+        clickedCard = null;
+        preventClick = false;
+      }, 500);
+    } else {
+      combosFound++;
+      clickedCard = null;
+      if (combosFound === 12) {
+        alert("YOU WIN");
+      }
+    }
+  }
+}
+
+function timer() {
+  let timeLeft = Number(document.getElementById("timer").innerText);
+  timeLeft--;
+  document.getElementById("timer").innerText = timeLeft;
+
+  if (timeLeft === 0) {
+    clearInterval(timerStart);
+    alert("Game Over");
+    stop();
+  }
+}
+
+function timerStart() {
+  setInterval(timer, 1000);
+}
